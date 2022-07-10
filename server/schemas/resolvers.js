514d0a35e8dfd5bbbd18375b24e5fb8f1b6daa83
thetryworks <natesmyth1@gmail.com>
2022-07-10 00:47:58 -0500

    added saveBook route

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,8 +39,16 @@ const resolvers = {
             return { token, user };
 
         },
-        saveBook: {
-
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                  { _id: context.user._id },
+                  { $addToSet: { savedBooks: input } },
+                  { new: true, runValidators: true }
+                );
+                return updatedUser;
+              }
+              throw new AuthenticationError("Please Login");
         },
         removeBook: {
 
