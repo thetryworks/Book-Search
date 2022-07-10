db5e369a28a640736735ed8e69d3f7d5780434f7
thetryworks <natesmyth1@gmail.com>
2022-07-10 15:28:14 -0500

    added useMutation to the search books.js

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -4,6 +4,8 @@ import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'reac
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from "../utils/mutations";
+import { useMutation } from "@apollo/react-hooks";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -20,6 +22,7 @@ const SearchBooks = () => {
     return () => saveBookIds(savedBookIds);
   });
 
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
