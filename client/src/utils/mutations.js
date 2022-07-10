8f50a187a410a0a5fec5a2b3c881632cf56e05eb
thetryworks <natesmyth1@gmail.com>
2022-07-10 09:56:57 -0500

    added mutations for adding and removing books

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,9 +25,38 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
+    mutation saveBook($input: SavedBookInput) {
+        saveBook(input: $input) {
+        username
+        _id
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+        }
+    }
 
 `
 
 export const REMOVE_BOOK = gql`
-
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+        _id
+        username
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+        }
+    }
 `
